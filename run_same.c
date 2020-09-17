0c1d008de39eced09eb50acafe52f8f2748cd6ed
jaewon1778 <jeawoun2001@gmail.com>
2020-09-17 15:52:41 +0900

    ch3 labs added

---
+++ run_same.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+	int x;
+	int y;
+	float pi;
+	float t;
+	
+	pi=3.14;
+
+	printf("속력을 입력하세요\n판다:");
+	scanf("%d",&x);
+
+	printf("말:");
+	scanf("%d",&y);
+
+	t=5*2*pi/(x+y);
+	printf("t=%f\n",t);
+}
